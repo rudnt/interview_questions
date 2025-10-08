50c2c4c4c6c73b974312b2ab3ee2e844bd2287ed
Tomasz Rudnicki <tomaszrudnicki95@gmail.com>
2025-10-08 21:31:27 +0200

    Add async calls to sources

--- src/problems/problems_repo.cppm
+++ src/problems/problems_repo.cppm
@@ -1,6 +1,7 @@
 module;
 
 #include <algorithm>
+#include <future>
 #include <map>
 #include <memory>
 #include <string>
@@ -12,15 +13,24 @@ import IProblemsPermStorage;
 export module ProblemsRepo;
 
 namespace interviews {
+    using ProblemsMap = std::map<std::string, std::string>;
+    using ProblemsUnorderedMap = std::unordered_map<std::string, std::string>;
+
     export class ProblemsRepo {
     public:
         ProblemsRepo(std::initializer_list<std::unique_ptr<IProblemsPermStorage>> sources = {});
 
-        std::map<std::string, std::string> getProblems(size_t limit = 0, size_t offset = 0) const;
-        //// TODO repo can use std::async to read from multiple sources concurrently - check how to do it (locking, mutexes, etc.)
+        ProblemsMap get(size_t maxSize = 0, size_t offset = 0) const;
 
     private:
         std::vector<std::unique_ptr<IProblemsPermStorage>> sources;
+
+        ProblemsMap fetch() const;
+        void erase(ProblemsMap&, size_t) const;
+        void limit(ProblemsMap&, size_t) const;
+
+        std::vector<std::future<ProblemsUnorderedMap>> fetchAsync() const;
+        ProblemsMap gather(std::vector<std::future<ProblemsUnorderedMap>>&) const;
     };
 }
 
@@ -32,25 +42,54 @@ namespace interviews {
         }
     }
 
-    std::map<std::string, std::string> ProblemsRepo::getProblems(size_t limit, size_t offset) const {
-        std::map<std::string, std::string> allProblems;
+    ProblemsMap ProblemsRepo::get(size_t maxSize, size_t offset) const {
+        auto problems{ fetch() };
+        erase(problems, offset);
+        limit(problems, maxSize);
+        return problems;
+    }
+
+    ProblemsMap ProblemsRepo::fetch() const {
+        auto futures{ fetchAsync() };
+        return gather(futures);
+    }
+
+    std::vector<std::future<ProblemsUnorderedMap>> ProblemsRepo::fetchAsync() const {
+        std::vector<std::future<ProblemsUnorderedMap>> futures;
+        futures.reserve(sources.size());
         for (const auto& source : sources) {
+            futures.push_back(std::async(std::launch::async, &IProblemsPermStorage::get, source.get()));
+        }
+        return futures;
+    }
+
+    ProblemsMap ProblemsRepo::gather(std::vector<std::future<ProblemsUnorderedMap>>& futures) const {
+        ProblemsMap allProblems;
+        for (auto& future : futures) {
             // TODO consider logging warning if problem duplicated
-            auto problems{ source->get() };
+            auto problems{ future.get() };
             allProblems.insert(problems.begin(), problems.end());
         }
-
-        if (offset != 0) {
-            auto it{ allProblems.begin() };
-            std::advance(it, std::min(offset, allProblems.size()));
-            allProblems.erase(allProblems.begin(), it);
-        }
-        if (limit != 0) {
-            auto it{ allProblems.begin() };
-            std::advance(it, std::min(limit, allProblems.size()));
-            allProblems.erase(it, allProblems.end());
-        }
-
         return allProblems;
+    }
+
+    void ProblemsRepo::erase(ProblemsMap& problems, size_t count) const {
+        if (count == 0) {
+            return;
+        }
+
+        auto it{ problems.begin() };
+        std::advance(it, std::min(count, problems.size()));
+        problems.erase(problems.begin(), it);
+    }
+
+    void ProblemsRepo::limit(ProblemsMap& problems, size_t count) const {
+        if (count == 0) {
+            return;
+        }
+
+        auto it{ problems.begin() };
+        std::advance(it, std::min(count, problems.size()));
+        problems.erase(it, problems.end());
     }
 }
--- tests/unit/problems/problems_repo.cpp
+++ tests/unit/problems/problems_repo.cpp
@@ -8,23 +8,21 @@
 
 import ProblemsRepo;
 
-//// TODO check how to check if async is used
-
 TEST_F(ProblemsRepoTest, GetProblems_NoSource_ReturnsEmptyCollection) {
     interviews::ProblemsRepo repo;
-    auto problems{ repo.getProblems() };
+    auto problems{ repo.get() };
     ASSERT_EQ(problems.size(), 0);
 }
 
 TEST_F(ProblemsRepoTest, GetProblems_EmptySource_ReturnsEmptyCollection) {
     auto repo{ getEmptySourceRepo() };
-    auto problems{ repo.getProblems() };
+    auto problems{ repo.get() };
     ASSERT_EQ(problems.size(), 0);
 }
 
 TEST_F(ProblemsRepoTest, GetProblems_SingleSource_ReturnsExpectedProblems) {
     auto repo{ getSingleSourceRepo() };
-    auto problems{ repo.getProblems() };
+    auto problems{ repo.get() };
     auto expected{ getData() };
     
     ASSERT_EQ(problems.size(), expected.size());
@@ -36,7 +34,7 @@ TEST_F(ProblemsRepoTest, GetProblems_SingleSource_ReturnsExpectedProblems) {
 
 TEST_F(ProblemsRepoTest, GetProblems_MultipleSources_AggregatesAllProblems) {
     auto repo{ getMultiSourceRepo() };
-    auto problems{ repo.getProblems() };
+    auto problems{ repo.get() };
     auto expected{ getData() };
     
     ASSERT_EQ(problems.size(), expected.size());
@@ -48,7 +46,7 @@ TEST_F(ProblemsRepoTest, GetProblems_MultipleSources_AggregatesAllProblems) {
 
 TEST_F(ProblemsRepoTest, GetProblems_MultipleSourcesSomeEmpty_AggregatesAllProblems) {
     auto repo{ getMultiSourceSomeEmptyRepo() };
-    auto problems{ repo.getProblems() };
+    auto problems{ repo.get() };
     auto expected{ getData() };
     
     ASSERT_EQ(problems.size(), expected.size());
@@ -61,8 +59,8 @@ TEST_F(ProblemsRepoTest, GetProblems_MultipleSourcesSomeEmpty_AggregatesAllProbl
 TEST_F(ProblemsRepoTest, GetProblems_RepoCopy_ReturnsConsistentResults) {
     auto repo1{ getSingleSourceRepo() };
     auto repo2{ getSingleSourceRepo() };
-    auto problems1{ repo1.getProblems() };
-    auto problems2{ repo2.getProblems() };
+    auto problems1{ repo1.get() };
+    auto problems2{ repo2.get() };
 
     ASSERT_EQ(problems1.size(), problems2.size());
     EXPECT_EQ(problems1, problems2);
@@ -70,7 +68,7 @@ TEST_F(ProblemsRepoTest, GetProblems_RepoCopy_ReturnsConsistentResults) {
 
 TEST_F(ProblemsRepoTest, GetProblems_DuplicatedNameInSources_ReturnsLastOccurrence) {
     auto repo{ getMultiSourceRepo() };
-    auto problems{ repo.getProblems() };
+    auto problems{ repo.get() };
     auto expected{ getData() };
     
     ASSERT_EQ(problems.size(), expected.size());
@@ -82,8 +80,8 @@ TEST_F(ProblemsRepoTest, GetProblems_DuplicatedNameInSources_ReturnsLastOccurren
 
 TEST_F(ProblemsRepoTest, GetProblems_Limited_ReturnsConsistentResults) {
     auto repo{ getSingleSourceRepo() };
-    auto problems1{ repo.getProblems(2) };
-    auto problems2{ repo.getProblems(2) };
+    auto problems1{ repo.get(2) };
+    auto problems2{ repo.get(2) };
 
     ASSERT_EQ(problems1.size(), problems2.size());
     EXPECT_EQ(problems1, problems2);
@@ -91,9 +89,9 @@ TEST_F(ProblemsRepoTest, GetProblems_Limited_ReturnsConsistentResults) {
 
 TEST_F(ProblemsRepoTest, GetProblems_Paginated_ReturnsConsistentResults) {
     auto repo{ getSingleSourceRepo() };
-    auto problems1{ repo.getProblems(2, 2) };
-    auto problems2{ repo.getProblems(2, 2) };
-    auto problems3{ repo.getProblems(1, 2) };
+    auto problems1{ repo.get(2, 2) };
+    auto problems2{ repo.get(2, 2) };
+    auto problems3{ repo.get(1, 2) };
 
     ASSERT_EQ(problems1.size(), problems2.size());
     EXPECT_EQ(problems1, problems2);
@@ -105,22 +103,22 @@ TEST_F(ProblemsRepoTest, GetProblems_Paginated_ReturnsConsistentResults) {
 
 TEST_F(ProblemsRepoTest, GetProblems_PaginationOutOfRange_ReturnsEmptyCollection) {
     auto repo{ getSingleSourceRepo() };
-    auto problems{ repo.getProblems(0, 5) };
+    auto problems{ repo.get(0, 5) };
     ASSERT_EQ(problems.size(), 0);
 }
 
 TEST_F(ProblemsRepoTest, GetProblems_LimitExceedsTotal_ReturnsAllProblems) {
     auto repo{ getSingleSourceRepo() };
-    auto expected{ repo.getProblems() };
-    auto limitedProblems{ repo.getProblems(1000) };
+    auto expected{ repo.get() };
+    auto limitedProblems{ repo.get(1000) };
     ASSERT_EQ(limitedProblems.size(), expected.size());
     EXPECT_EQ(limitedProblems, expected);
 }
 
 TEST_F(ProblemsRepoTest, GetProblems_ZeroLimit_ReturnsWholeCollection) {
     auto repo{ getSingleSourceRepo() };
-    auto expected{ repo.getProblems() };
-    auto problems{ repo.getProblems(0) };
+    auto expected{ repo.get() };
+    auto problems{ repo.get(0) };
     ASSERT_EQ(problems.size(), expected.size());
     EXPECT_EQ(problems, expected);
 }
@@ -137,7 +135,7 @@ TEST_F(ProblemsRepoTest, ThreadSafety_ConcurrentAccess) {
             for (size_t j{ 0 }; j != calls_per_thread; ++j) {
                 try {
                     auto repo{ getMultiSourceRepo() };
-                    auto problems{ repo.getProblems()};
+                    auto problems{ repo.get()};
                     if (!problems.empty()) {
                         successful_calls++;
                     }
